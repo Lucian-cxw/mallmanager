547f9480fec5f46a7d2e21e6dd2ff91b7aca98e2
Lucian-cxw <616730488@qq.com>
2021-04-30 23:08:27 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld' 无用代码，删除
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      // name: 'HelloWorld', 无用代码删除
+      // component: HelloWorld  无用代码删除 整个组件也删除了
+    }
+  ]
+})
