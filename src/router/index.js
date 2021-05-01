08459cdff1b4e0f2f0265e62f87ce20a79bba807
Lucian-cxw <616730488@qq.com>
2021-05-01 11:07:01 +0800

    新建登录分支新建登录组件配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,24 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld' 无用代码，删除
 
+// 引入login包
+// import Login from "../components/login/login.vue"  这样写没问题
+//  以下写法为webpack 提供的@ 可以直接找到SRC文件夹
+import Login from "@/components/login/login.vue" 
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   // name: 'HelloWorld', 无用代码删除
+    //   // component: HelloWorld  无用代码删除 整个组件也删除了
+    // }
     {
-      path: '/',
-      // name: 'HelloWorld', 无用代码删除
-      // component: HelloWorld  无用代码删除 整个组件也删除了
+      name:"login",
+      path:"/login",
+      component:Login
     }
   ]
 })
