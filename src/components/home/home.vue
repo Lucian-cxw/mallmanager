c1e667114a451d7b932e9323d76d93d14b936e44
Lucian-cxw <616730488@qq.com>
2021-05-02 13:41:31 +0800

    引入侧边栏导航组件，调整样式

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -8,7 +8,7 @@
                 </div>
             </el-col>
             <el-col :span="18" class="middle">
-               <h3>电商管理系统</h3>
+                <h3>电商管理系统</h3>
             </el-col>
             <el-col :span="2">
                 <div class="grid-content bg-purple">
@@ -18,7 +18,75 @@
         </el-row>
     </el-header>
     <el-container>
-        <el-aside class="aside" width="200px">Aside</el-aside>
+        <!-- 侧边栏导航组件 -->
+        <el-aside class="aside" width="200px">
+            <el-menu unique-opened="true">
+                <!-- 1 -->
+                <el-submenu index="1">
+                      <!-- <template slot="title">分组一</template> -->
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>用户管理</span>  
+                    </template>   
+                       <el-menu-item index="1-1">
+                           <i class="el-icon-menu"></i> 
+                           用户列表</el-menu-item>            
+                </el-submenu>
+                <!-- 2 -->
+                  <el-submenu index="2">
+                      <!-- <template slot="title">分组一</template> -->
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>权限管理</span>  
+                    </template>   
+                       <el-menu-item index="1-1">
+                           <i class="el-icon-menu"></i> 
+                           角色管理</el-menu-item>    
+                            <el-menu-item index="1-1">
+                           <i class="el-icon-menu"></i> 
+                           权限列表</el-menu-item>           
+                </el-submenu>
+                <!-- 3 -->
+                  <el-submenu index="3">
+                      <!-- <template slot="title">分组一</template> -->
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>商品管理</span>  
+                    </template>   
+                       <el-menu-item index="1-1">
+                           <i class="el-icon-menu"></i> 
+                           商品列表</el-menu-item>     
+                           <el-menu-item index="1-1">
+                           <i class="el-icon-menu"></i> 
+                           分类参数</el-menu-item>  
+                           <el-menu-item index="1-1">
+                           <i class="el-icon-menu"></i> 
+                           商品分类</el-menu-item>         
+                </el-submenu>
+                <!-- 4 -->
+                  <el-submenu index="4">
+                      <!-- <template slot="title">分组一</template> -->
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>订单管理</span>  
+                    </template>   
+                       <el-menu-item index="1-1">
+                           <i class="el-icon-menu"></i> 
+                           订单列表</el-menu-item>            
+                </el-submenu>
+                <!-- 5 -->
+                  <el-submenu index="5">
+                      <!-- <template slot="title">分组一</template> -->
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>数据统计</span>  
+                    </template>   
+                       <el-menu-item index="1-1">
+                           <i class="el-icon-menu"></i> 
+                           数据报表</el-menu-item>            
+                </el-submenu>
+            </el-menu>
+        </el-aside>
         <el-main class="main">Main</el-main>
     </el-container>
 </el-container>
@@ -47,11 +115,13 @@ export default {
     background-color: #e9eef3;
     height: 100%;
 }
+
 /* 头部样式 */
-.middle{
+.middle {
     text-align: center;
 }
-.loginout{
+
+.loginout {
     line-height: 60px;
     text-decoration: none;
 }
